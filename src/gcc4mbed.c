8c8fbb59db7872435a44667fd9c9b107ef57e56e
Adam Green <adamgr@foo.bar>
2013-02-11 00:33:10 -0800

    Fix _sbrk() and update license.
    The license on gcc4mbed.c should have been Apache v2 and not the LGPL.
    
    I was losing 2k of the heap by initializing _sbrk() to use __HeapLimit
    instead of __end__.

--- src/gcc4mbed.c
+++ src/gcc4mbed.c
@@ -1,17 +1,16 @@
-/* Copyright 2012 Adam Green (http://mbed.org/users/AdamGreen/)
+/* Copyright 2013 Adam Green (http://mbed.org/users/AdamGreen/)
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published
-   by the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
+       http://www.apache.org/licenses/LICENSE-2.0
 
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 /* Provide routines which hook the MRI debug monitor into GCC4MBED projects. */
 #include <string.h>
@@ -132,7 +131,7 @@ void __cxa_pure_virtual(void)
 
 
 /* Linker defined symbol to be used by sbrk for where dynamically heap should start. */
-int __HeapLimit;
+int __end__;
 
 /* Turn off the errno macro and use actual external global variable instead. */
 #undef errno
@@ -141,7 +140,7 @@ extern int errno;
 /* Dynamic memory allocation related syscalls. */
 caddr_t _sbrk(int incr) 
 {
-    static unsigned char* heap = (unsigned char*)&__HeapLimit;
+    static unsigned char* heap = (unsigned char*)&__end__;
     unsigned char*        prev_heap = heap;
     unsigned char*        new_heap = heap + incr;
 
