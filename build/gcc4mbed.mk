2290b63deab9989b7c30d08f4c8a46e9db0d3480
Larry Littlefield <svkatielee@yahoo.com>
2014-11-14 16:26:04 +0800

    Fix deploy for other devices

--- build/gcc4mbed.mk
+++ build/gcc4mbed.mk
@@ -329,7 +329,7 @@ clean-all: clean
 	@echo Cleaning $(LIB_DEBUG_DIR)
 	$(Q) $(REMOVE_DIR) $(call convert-slash,$(LIB_DEBUG_DIR)) $(QUIET)
 
-deploy: LPC1768-deploy
+deploy: (DEVICES)-deploy$
 
 
 # Determine supported devices by looking at *-device.mk makefiles.
